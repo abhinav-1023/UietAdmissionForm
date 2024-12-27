ef0962b8ddc896c10e648270a6279583d965b23f
abhinav-1023 <132914298+abhinav-1023@users.noreply.github.com>
2024-12-27 14:44:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+
+
 app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
