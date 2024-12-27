13e5ec8debacf2cd9a555cd8f32ed24b4ee9b4cf
abhinav-1023 <132914298+abhinav-1023@users.noreply.github.com>
2024-12-27 17:43:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
   {
-  origin:[""],
+  origin:["https://uiet-admission-form-api.vercel.app"],
   method:["POST","GET"],
   credentials:true
   }
