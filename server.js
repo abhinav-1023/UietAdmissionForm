b64326cc1db574c064b62b1cdcc024f676ebd2f1
abhinav-1023 <132914298+abhinav-1023@users.noreply.github.com>
2024-12-27 10:04:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -123,6 +123,6 @@ app.get("/", (req, res) => {
 // Start Server
 mongoose.connection.once("open", () => {
   app.listen(3000, () => {
-    console.log("Server started on http://localhost:3000");
+    console.log("Server started on https://uiet-admission-form.vercel.app");
   });
 });
