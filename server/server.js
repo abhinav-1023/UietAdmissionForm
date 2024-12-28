1f0917124f1200f7dae892cd95b5ffd2ca43a9e3
abhinav-1023 <132914298+abhinav-1023@users.noreply.github.com>
2024-12-28 14:27:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type','Authorization'],
-  credentials: true // Allow credentials (cookies, authorization headers)
+  credentials: false // Allow credentials (cookies, authorization headers)
 }));
 
 app.use("/uploads", express.static("uploads")); // Serve static files
