606d143ae0c035071b9cd5a48f5614944c8e4bda
abhinav-1023 <132914298+abhinav-1023@users.noreply.github.com>
2024-12-29 15:56:58 +0530

    Delete api directory

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import { createServer } from 'http';
-import app from '../server/server';  // Import your server.js from the server folder
-
-export default function handler(req, res) {
-  // Use the Express app for handling requests
-  app(req, res);  // Forward requests to your Express app
-}
