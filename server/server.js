ce13e9f18de4de5a8c14153cfcd12d771c74e13c
abhinav-1023 <132914298+abhinav-1023@users.noreply.github.com>
2024-12-29 15:04:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -125,8 +125,10 @@ app.post(
 // Test Route
 app.get('/', (req, res) => { res.send('Welcome to the UIET Admission Form API!'); })
 // Start Server
-const PORT = 3000; // You can change this to any port number you prefer 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); }); });
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running...');
+});
+
 
 
 mongoose.connection.once("open", () => {
